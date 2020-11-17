2d017461b6c62bdc1747c3034f7ad3be05a7a6f7
sanghun0724 <sanghun0724@naver.com>
2020-11-18 00:58:57 +0900

    d

--- CustomLogin/CustomLogin/ViewController/SignUpViewController.swift
+++ CustomLogin/CustomLogin/ViewController/SignUpViewController.swift
@@ -9,23 +9,20 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
-    @IBO
-
+    @IBOutlet weak var firstNameTextField:UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpTapped(_ sender: Any) {
     }
-    */
-
+    
 }
